c339726d64aa4af398e38475dfccdb944262b58b
TarochanChannel <gakutyn.2020@gmail.com>
2021-02-23 17:45:58 +0900

    整備...整備...整備...

---
+++ windows-builder/main.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const fs = require("fs");
+const mkdirp = require('mkdirp');
+const { execSync } = require('child_process');
+
+var git = "https://raw.githubusercontent.com/TarochanChannel/lainan-for-discord/main/";
+var files;
+request(git + "app-files.json", function (error, response, body) {
+    if (error || !body) return;
+    files = JSON.parse(body);
+    Object.keys(files.windows).forEach(function (folder) {
+        files.windows[folder].forEach(function (file) {
+            request(`${git}${folder}/${file}`, function (error, response, body) {
+                if (error || !body) return;
+                mkdirp.sync(`${folder}`);
+                fs.writeFileSync(`${folder}/${file}`, body);
+            });
+        });
+    });
+
+    execSync("cd app&&npm install");
+    execSync("npx electron-builder -w --projectDir app");
+});
